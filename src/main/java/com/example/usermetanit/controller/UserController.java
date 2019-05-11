dcafbfd7541e3501c10db2c7f99ee1536a55608d
Oleg <cpabox@gmail.com>
2019-05-11 21:48:07 +0300

    Just commit it, baby.

--- src/main/java/com/example/usermetanit/controller/UserController.java
+++ src/main/java/com/example/usermetanit/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class UserController {
+
     @Autowired
     UserRepo userRepo;
 
@@ -16,18 +18,25 @@ public class UserController {
     public List<User> getAllUsers() {
         return (List<User>) userRepo.findAll();
     }
+
     @GetMapping("/users/{id}")
     public User getUserById(@PathVariable int id) {
         return userRepo.findById(id).get();
     }
+
     @PostMapping("/users")
     public User addUser(@RequestBody User user) {
         return userRepo.save(user);
     }
-    @PutMapping("/users")
-    public User updateUser(@RequestBody User user) {
-        return userRepo.save(user);
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable int id) {
+        User updateUser = userRepo.findById(id).get();
+        updateUser.setAge(user.getAge());
+        updateUser.setName(user.getName());
+        return userRepo.save(updateUser);
     }
+
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id) {
         userRepo.deleteById(id);
